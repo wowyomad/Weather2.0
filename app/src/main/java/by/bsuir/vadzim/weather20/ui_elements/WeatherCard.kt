0c66353ab85111707d2b3aefe87069be0018705f
wowyomad <vadimsun8837@gmail.com>
2023-11-12 01:06:57 +0300

    added red color to favorite icon on WeatherCard and made it scale to 1.2f when isFavorite is true using animateAsFloat

--- app/src/main/java/by/bsuir/vadzim/weather20/ui_elements/WeatherCard.kt
+++ app/src/main/java/by/bsuir/vadzim/weather20/ui_elements/WeatherCard.kt
@@ -1,11 +1,19 @@
 package by.bsuir.vadzim.weather20.ui_elements
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,11 +27,18 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -35,8 +50,9 @@ import by.bsuir.vadzim.weather20.database.WeatherInfo
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun WeatherCard (weather: WeatherInfo, onEvent: (WeatherEvent) -> Unit){
+fun WeatherCard(weather: WeatherInfo, onEvent: (WeatherEvent) -> Unit) {
     val shape = RoundedCornerShape(12.dp)
+    val  interactionSource: InteractionSource = MutableInteractionSource()
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -49,7 +65,8 @@ fun WeatherCard (weather: WeatherInfo, onEvent: (WeatherEvent) -> Unit){
             )
     ) {
         Column(
-            modifier = Modifier.weight(1.25f)
+            modifier = Modifier
+                .weight(1.25f)
                 .padding(horizontal = 16.dp)
                 .align(Alignment.CenterVertically)
         ) {
@@ -61,7 +78,7 @@ fun WeatherCard (weather: WeatherInfo, onEvent: (WeatherEvent) -> Unit){
                 Text(text = stringResource(id = weather.type.name), fontSize = 12.sp)
             }
         }
-        
+
         Text(
             text = weather.description,
             fontSize = 16.sp,
@@ -72,19 +89,37 @@ fun WeatherCard (weather: WeatherInfo, onEvent: (WeatherEvent) -> Unit){
                 .align(Alignment.Top)
                 .padding(top = 12.dp)
         )
-
+        val interactionSource = remember { MutableInteractionSource() }
+        val pressed by interactionSource.collectIsPressedAsState()
         IconButton(
+            interactionSource = interactionSource,
             modifier = Modifier
                 .align(Alignment.CenterVertically)
                 .padding(10.dp),
             onClick = {
                 onEvent(WeatherEvent.Favorite(weather))
             }) {
-            Image(imageVector =
-            if (!weather.isFavorite) Icons.Default.FavoriteBorder
-            else Icons.Default.Favorite,
-                contentDescription = "Add to Favorite"
+            val scale by animateFloatAsState(
+                targetValue = if (weather.isFavorite) 1.2f else 1f
             )
+            val animationSpec = tween<Float>(durationMillis = 300, easing = LinearOutSlowInEasing)
+
+
+            Image(
+                modifier = Modifier
+                    .graphicsLayer(scaleX = scale, scaleY = scale)
+                    ,
+                imageVector =
+                if (!weather.isFavorite) Icons.Default.FavoriteBorder
+                else Icons.Default.Favorite,
+                contentDescription = "Add to Favorite",
+                colorFilter = ColorFilter.tint(
+                    if(weather.isFavorite) Color.Red
+                    else if (isSystemInDarkTheme()) Color.White
+                    else Color.Black
+                )
+            )
+
         }
 
     }
